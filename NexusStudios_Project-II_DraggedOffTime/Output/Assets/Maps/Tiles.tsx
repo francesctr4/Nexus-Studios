fefcd38712e59ea21a711487df0c32a3f9c89e2f
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2023-04-18 15:02:12 +0200

    Map fixed

--- NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/Tiles.tsx
+++ NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/Tiles.tsx
@@ -1,4 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Tiles" tilewidth="16" tileheight="16" tilecount="625" columns="25">
+<tileset version="1.10" tiledversion="1.10.0" name="Tiles" tilewidth="16" tileheight="16" tilecount="625" columns="25">
  <image source="Tiles.png" width="400" height="400"/>
+ <wangsets>
+  <wangset name="Ladrillo" type="corner" tile="30">
+   <wangcolor name="Esquinas" color="#ff0000" tile="56" probability="1"/>
+   <wangcolor name="Centro" color="#00ff00" tile="30" probability="1"/>
+   <wangtile tileid="4" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="6" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="7" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="8" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="9" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="29" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="30" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="31" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="32" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="33" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="34" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="54" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="55" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="56" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="57" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="58" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="59" wangid="0,2,0,2,0,2,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
