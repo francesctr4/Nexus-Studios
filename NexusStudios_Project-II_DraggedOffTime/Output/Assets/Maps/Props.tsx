eb8418df66442ffac3e527f227ae43304aea6cda
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2023-04-18 23:31:53 +0200

    ChestRoom added

--- NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/Props.tsx
+++ NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/Props.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Props" tilewidth="16" tileheight="16" tilecount="625" columns="25" objectalignment="topleft">
+<tileset version="1.10" tiledversion="1.10.0" name="Props" tilewidth="16" tileheight="16" tilecount="625" columns="25">
  <image source="Props.png" width="400" height="400"/>
 </tileset>
