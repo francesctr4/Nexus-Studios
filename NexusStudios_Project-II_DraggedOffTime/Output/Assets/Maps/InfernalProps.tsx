c663878becb49f46c42dfa83b85276854c381ae2
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2023-05-12 20:43:47 +0200

    Maps Added

---
+++ NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/InfernalProps.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="InfernalProps" tilewidth="16" tileheight="16" tilecount="625" columns="25">
+ <image source="InfernalProps.png" width="400" height="400"/>
+</tileset>
---
+++ NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/enemy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Damage" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="../Textures/Damage.png" width="128" height="32"/>
+</tileset>
