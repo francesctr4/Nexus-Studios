5863cc3ffcdd00787627cfb3454b6bea57a39cda
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2023-05-14 02:06:00 +0200

    Maps Fixed

--- NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/InfernalTiles.tsx
+++ NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/InfernalTiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="InfernalTiles" tilewidth="16" tileheight="16" tilecount="625" columns="25">
+<tileset version="1.10" tiledversion="1.10.0" name="InfernalTilesMalo" tilewidth="16" tileheight="16" tilecount="625" columns="25">
  <image source="InfernalTiles.png" width="400" height="400"/>
  <wangsets>
   <wangset name="Suelo" type="corner" tile="-1">
