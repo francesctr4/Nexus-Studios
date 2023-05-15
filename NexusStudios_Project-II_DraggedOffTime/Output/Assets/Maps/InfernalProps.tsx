ad988b723ad68f16b139c569e90c95cbda46aded
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2023-05-15 20:52:08 +0200

    All maps added

--- NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/InfernalProps.tsx
+++ NexusStudios_Project-II_DraggedOffTime/Output/Assets/Maps/InfernalProps.tsx
@@ -1,4 +1,2 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="InfernalProps" tilewidth="16" tileheight="16" tilecount="625" columns="25">
- <image source="InfernalProps.png" width="400" height="400"/>
-</tileset>
+<tileset version="1.10" tiledversion="1.10.0" name="Props" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
